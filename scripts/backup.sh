d86500150b1d0ba1b0724c421dfb4a5e2001747f
mcavady <james@zeropointproductions.co.uk>
2014-05-08 19:01:30 +0100

    Created script folder

---
+++ scripts/backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# dirs to backup
+  backup_files="/www /sites-enabled /sites-available"
+#save zip file else were on the server, where <USERNAME> is your user name.
+  dest="/home/<USERNAME>/backups"
+#Create archive file name
+  day=$(date +%d)
+  month=$(date +%b )
+  year=$(date +%y)
+
+# Set your www directory
+  prefix=""
+
+# Set the file names for the zip with the $prefix and the day month then year
+  archive_file="$prefix-$day-$month-$year.tgz"
+#Print start status
+  echo "Backing it all up $backup_files to $dest/$archive_file"
+    date
+  echo
+    tar czf $dest/$archive_file $backup_files
+  echo
+# print end message
+  echo "backup complete : " date
+#long listing of the files in dest
+  ls -lh $dest
+#mail out a report and mail (sendMail only works if the user is logged and runs the script, run perms I think)
+  echo "www backup completed \n Date of Backup : $day $month $year \n Dirs backed up : $backup_files \n Time of the backup : $(date) \n Backup location : $dest" | sendmail Backup blar@blar.co.uk
---
+++ scripts/sql.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#sql backup user, user name and password for the mysql user you want to use this script
+  DB_USER=""
+  DB_PASSWD=""
+
+#optimise tables and check
+
+  mysqlcheck --user=$DB_USER --password=$DB_PASSWD --all-databases;
+
+# sql to backup use the user for my sql and dumps the file to a tmp directory
+  mysqldump --user=$DB_USER --password=$DB_PASSWD something > something.sql;
+
+#the directories that you want to include
+  backup_files="/files "
+
+#save zip file else were on the server
+  dest="/backups"
+
+#Create archive file name
+  day=$(date +%d)
+  month=$(date +%b )
+  year=$(date +%y)
+  prefix="sql"
+
+  archive_file="$prefix-$day-$month-$year.tgz"
+
+#Print start status
+  echo "Backing it all up $backup_files to $dest/$archive_file"
+    date
+  echo
+    tar czf $dest/$archive_file $backup_files
+  echo
+
+# print end message
+  echo "Backup complete : " date
+
+#long listing of the files in dest
+  ls -lh $dest
+
+#remove all sql temp files
+  rm *.sql
+
+#mail out a report if running from the term window
+#  echo "sql backup completed \n Date of Backup : $day $month $year \n Backed up : SQL \n Time of the backup : $(date) \n Backup location : $dest" | sendmail Backup blar@blar.co.uk
+
+From: me@blar.co.uk
+To: me@blar.co.uk
+Subject: MIME Test
+
+cat <<EOF
+Mime-Version: 1.0
+Content-Type: text/html
+
+<!doctype html>
+<html>
+    <head>
+    <meta charset='utf-8'>
+    <title>Backups for sql complete</title>
+      <style type='text/css'>
+      /*styles to go here*/
+      .main{width:100%;background:#000;}
+      .heading{width:95%;background:#ddd;}
+      .information{width:95%;background:#888;}
+      </style>
+    </head>
+<body>
+  <div class='main'>
+    <div class='heading'>
+      <h1>SQL optimised and backed up $day - $month - $year</h1>
+        <div class='information'>
+	dest: $dest
+        </div>
+    </div>
+  </div>
+</body>
+</html>
+EOF
+
+
